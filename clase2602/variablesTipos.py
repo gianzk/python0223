efbc9b894f8aefae646f4d8b4d17f5cdd4569498
gianzk <g.gutierrez@uni.pe>
2023-02-26 11:34:46 -0500

    primer cambio

---
+++ clase2602/variablesTipos.py
@@ -0,0 +1,46 @@
+### variables 
+### recomendaciones no iniciar con numeros
+## sin espacios ni caracteres especiales
+message="hola"
+edad=20
+message_de_saludo=" hola "
+
+## tipos de datos
+entero=21
+flotante=3.14
+unValorLogico=True
+unacadenadetexto="cadena de texto"
+### cadena de texto multilineas 3 comillas
+literalCadena="""
+    Hola como 
+    estas 
+    sigo sin error 
+"""
+## carateres de escape \t-> tabulacion \n  ->separacion de linea
+
+cadenaSaludo="Hola"
+persona="Gianmarco"
+saludoPersonalizado=cadenaSaludo+persona
+print(saludoPersonalizado)
+print("linea de comparacion")
+print(cadenaSaludo,persona)
+# cantidad de caratecteres de una cadena
+sizeCadena=len(saludoPersonalizado)
+print(len(saludoPersonalizado))
+print("linea de comparacion")
+print(sizeCadena)
+print("linea de comparacion")
+print(type(flotante))
+print(type(saludoPersonalizado))
+print(type(flotante)==float) # comparacion de tipos , para los otros serian str, int ,...
+# int() -> convierte otro tipo de dato a entero
+edad2="3"
+print(edad2,"=>",type(edad2))
+print("linea de comparacion")
+edadInt=int(edad2)
+print(edadInt,"=>",type(edadInt))
+a=3
+b=3
+
+c=(a==b)
+print(c)
---
+++ definciones.py
@@ -0,0 +1,13 @@
+#consideraciones para crear un programa archivo de python
+# nombre del archivo inicar en letras y al terminar puede ser un numero
+# no iniciar en numeros (opcional)
+# sin espacios 
+# sin caracteres especiales
+# EJEMPLOS QUE NO HACER : *PROGRAMA.PY
+### entorno de trabajo
+## python en cualquier version desde la 3.9 en adelante
+## visual studio code o anaconda 
+## Cuenta de github 
+## git instalado y github desktop
+## material ->https://github.com/gdelgador/PythonFundamentos
+print("Hello world")
