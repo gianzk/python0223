807562ab082fcac88ac6010eabf352a8d7390622
gianzk <g.gutierrez@uni.pe>
2023-03-05 11:50:25 -0500

    update

--- clase0503/ejercicio1.py
+++ clase0503/ejercicio1.py
@@ -1,4 +1,6 @@
 ### Minisistema
+roles=['admin','vendedor','inventario']
+
 sistema={
     "nombre":"tienda",
     "usuarios":[
@@ -6,7 +8,7 @@ sistema={
             'name':"",
             'password':"",
             'rol':"" ##vendedor ,administrador ,inventario
-        },
+        }
     ],
     "sedes":[{
         "nombreSede":"",
@@ -17,24 +19,65 @@ sistema={
         "nombre":"",
         "precio":"",
         "stock":""
-    },
-
-    ]
+    }]
 }
 ##### funciones
+## karen vera
 def agregarUsuario():
-    pass
+    nameUsuario=input("ingrese el nombre de usuario")
+    password=input("ingrese su password")
+    while True:
+        rol=input("ingrese su rol")
+        if rol in roles:
+            break
+        else:
+            print("ingrese un rol correcto",roles)
+    
+    dictUser={
+        'name':nameUsuario,
+        'password':password,
+        'rol':rol
+    }
+    sistema['usuarios'].append(dictUser)
+###
 def eliminarUsuario():
+    usuarioPorEliminar=input("ingrese usuario por eliminar")
+    for i,valor in enumerate(sistema['usuarios']):
+        if valor['name']==usuarioPorEliminar:
+                ## ingresar password para verificar que es correcto
+                sistema['usuarios'].remove(i)
+    ## remove
     pass
-def verificarRol():
-    pass
+###
+def obtenerRol(usuario):
+    rol=""
+    for i,valor in enumerate(sistema["usuarios"]):
+        if valor['name']==usuario:
+            rol=valor['rol']
+    return rol
+####
 def agregarSedes():
-    pass
+    usuario=input("ingresa usuario")
+    rol=obtenerRol(usuario)
+    if rol=='admin':
+        sede=input("ingrese sede")
+        ubicacion=input("ingrese ubicacion")
+        dictSede={
+            'sede':sede,
+            'ubicacion':ubicacion
+        }
+        sistema["sedes"].append(dictSede)
+    else:
+        print("no es un rol permitido")
+###
 def verSedes():
     pass
+#####
 def agregarProductos():
     pass
+#####
 def cambiarStock():
     pass
 
-
+agregarUsuario()
+print(sistema)
